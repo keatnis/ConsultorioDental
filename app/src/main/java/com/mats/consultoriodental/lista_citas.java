b924638056a19e28cc7e695b9e193e7d6b954cff
Maria <s17120026@itsm-tlapa.edu.mx>
2022-02-04 20:20:23 -0600

    creación de interfaces

---
+++ app/src/main/java/com/mats/consultoriodental/lista_citas.java
@@ -0,0 +1,14 @@
+package com.mats.consultoriodental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lista_citas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_citas);
+    }
+}
