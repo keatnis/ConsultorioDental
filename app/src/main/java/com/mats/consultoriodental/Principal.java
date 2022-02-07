91963be5d9dd9430aaa4f30e3bc85f4d47daf465
Maria <s17120026@itsm-tlapa.edu.mx>
2022-02-06 20:37:32 -0600

    boton entrar y reedireccionar los cardview

--- app/src/main/java/com/mats/consultoriodental/Principal.java
+++ app/src/main/java/com/mats/consultoriodental/Principal.java
@@ -1,14 +1,44 @@
 package com.mats.consultoriodental;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Principal extends AppCompatActivity {
+
+    CardView cvagendar;
+    CardView cvVer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+
+         cvagendar =findViewById(R.id.cvAgendar);
+
+         cvagendar.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent visor=new Intent(getApplicationContext(), RegistrarCita.class);
+                 startActivity(visor);
+             }
+         });
+
+        cvVer =findViewById(R.id.cvVer);
+
+        cvVer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent visor=new Intent(getApplicationContext(), lista_citas.class);
+                startActivity(visor);
+            }
+        });
+
+
+
     }
 }
